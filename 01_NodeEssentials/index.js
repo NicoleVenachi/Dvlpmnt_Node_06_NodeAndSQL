9944722712e54337f8819b715526ba57e467bd7a
nicolevenachi <ludwing@unicauca.edu.co>
2024-05-03 16:15:04 -0500

    Node Essentials -> Res object exploring

--- 01_NodeEssentials/index.js
+++ 01_NodeEssentials/index.js
@@ -5,6 +5,24 @@ const hostname = 'localhost';
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+  const {url} = req;
+  console.log(url);
+
+  if (url === '/translations') {
+    const translations = {
+      1: 'one',
+      2: 'two',
+      3: 'three'
+    }
+
+    //hheader del response para JSON
+    res.setHeader('Content-Type', 'application/json');
+
+    //data Tx (stream of data)
+    res.write(JSON.stringify(translations));
+
+    res.end();
+  }
   res.end('Welcome to Node!')
 })
 
